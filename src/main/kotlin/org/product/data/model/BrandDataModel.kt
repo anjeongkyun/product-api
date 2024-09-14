72c3359d0ebcba2a1a642269c2ab0a864d3e25a2
anjeongkyun <dkswjdrbs12@naver.com>
2024-09-14 18:18:12 +0900

    BrandDataModel를 정의한다

---
+++ src/main/kotlin/org/product/data/model/BrandDataModel.kt
@@ -0,0 +1,12 @@
+package org.product.data.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "brands")
+data class BrandDataModel(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+    @Column(unique = true)
+    val name: String,
+)
